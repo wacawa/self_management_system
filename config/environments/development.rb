53e1552d5cc177d25cea4cbddb59805718a3b580
wacawa <mikkunmitsuaki@gmail.com>
2023-05-18 15:07:59 +0900

    re-deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "self-manage-sys.fly.dev"
+  config.hosts << "self-management-system.fly.dev"
 
 end
