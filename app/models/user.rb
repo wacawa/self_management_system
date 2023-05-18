10016def2bb3bcae4f710110083a7bab665a8e4b
wacawa <mikkunmitsuaki@gmail.com>
2023-05-18 19:16:15 +0900

    add validate for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.emial = email.downcase }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
+  has_secure_password
+  validates :password, presence: true
+
 end
