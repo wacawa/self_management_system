d4c2c204962411ce0cf5bb60118215fe6834c2da
wacawa <mikkunmitsuaki@gmail.com>
2023-05-22 10:39:52 +0900

    add users contorller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: %i(new)
+
+  def create
+  end
+
+
+  # before_action
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
