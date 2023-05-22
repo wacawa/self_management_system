1d5814867a03dea8a1378c09d6662c363cabeb89
wacawa <mikkunmitsuaki@gmail.com>
2023-05-22 10:38:11 +0900

    make login and signup modal

--- app/javascript/static_pages/home.js
+++ app/javascript/static_pages/home.js
@@ -1,12 +1,24 @@
-let loginBtn = document.querySelector(".loginBtn")
-let loginModal = document.querySelector(".loginModal")
-
-loginBtn.addEventListener("click", function(){
-  loginModal.classList.add("open")
+getElem(".modalOpen", true).forEach((elem)=>{
+  elem.addEventListener("click", function(){
+    let classes = this.classList;
+    if(classes.contains("loginBtn")){
+      getElem(".loginModal").classList.add("open")
+    }else if(classes.contains("signupBtn")){
+      getElem(".signupModal").classList.add("open")
+    }
+  })
 })
 
-let closeBtn = document.querySelector(".closeBtn")
-
-closeBtn.addEventListener("click", function(){
-  loginModal.classList.remove("open")
+getElem(".modalClose", true).forEach((elem)=>{
+  elem.addEventListener("click", function(){
+    this.closest(".open").classList.remove("open")
+  })
 })
+
+function getElem(selector, getAll = false, scope = document){
+  if(!getAll){
+    return scope.querySelector(selector);
+  }else{
+    return scope.querySelectorAll(selector);
+  }
+}
