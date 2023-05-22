c8f51216c1c06e311c85c91d1ec067a7742af810
wacawa <mikkunmitsuaki@gmail.com>
2023-05-22 10:36:23 +0900

    setting route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
+
+  def login
+  end
+
+  def logout
+  end
+
+  def schedule
+  end
+
+  def skill
+  end
+
+  def money
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  resources :users
+  post 'login', to: 'static_pages#login'
+  delete 'logout', to: 'static_pages#logout'
+
+  get 'schedule', to: 'static_pages#schedule'
+  get 'skill', to: 'static_pages#skill'
+  get 'money', to: 'static_pages#money'
+
+  resources :users, except: :new
 end
