291e2a601a081070690b7a6e8496fdcc905e1a75
wacawa <mikkunmitsuaki@gmail.com>
2023-05-22 10:38:46 +0900

    edit host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "self-management-system.fly.dev"
+  config.hosts << "patient-sound-3309.fly.dev"
 
 end
